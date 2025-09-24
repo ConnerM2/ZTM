db545971999c26d2d59728f7339fa8bfe5682fa6
ConnerM Windows <manwillconner3@gmail.com>
2025-09-24 16:28:23 -0600

    Half way through basics of Python

--- pythonBasics.py
+++ pythonBasics.py
@@ -7,3 +7,18 @@ print(type('Hello'))
 snake_case = 'variabe names'
 PI = 3.14 # Constants are not meant to change
 a,b,c = 1,2,3
+
+nums = '1234567'
+# [start: stop: stepover]
+print(nums[:5])
+print(nums[2:])
+print(nums[::1])
+print(nums[::-1])
+
+birth_year = int(input("what is your birth year?: "))
+print(f"You are {2025 - birth_year} years old")
+
+user_name = input("Username: ")
+password = input("Password: ")
+password = "*" * len(password)
+print(f"{user_name}, your password {password} is {len(password)} letters long")
