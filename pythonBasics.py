0ebef34a4444219f67e0a7656e94a9829c379d1d
ConnerM Windows <manwillconner3@gmail.com>
2025-09-24 08:05:48 -0600

    First few lessons

---
+++ pythonBasics.py
@@ -0,0 +1,9 @@
+print("testing")
+
+print(type(54)) # Int
+print(type(54.5)) # Float
+print(type('Hello'))
+
+snake_case = 'variabe names'
+PI = 3.14 # Constants are not meant to change
+a,b,c = 1,2,3
